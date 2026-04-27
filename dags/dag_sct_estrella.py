38000ad8135798b4ac1d5e16f4c01efa34a0bcfe
cristian1255 <cmeneses394@gmail.com>
2026-04-27 08:17:35 -0600

    Change Postgres connection ID in DAG

--- dags/dag_sct_estrella.py
+++ dags/dag_sct_estrella.py
@@ -39,7 +39,7 @@ with DAG(
 
     crear_tablas = PostgresOperator(
         task_id="crear_tablas",
-        postgres_conn_id="postgres_datos_viales",
+        postgres_conn_id="postgres_viales",
         execution_timeout=timedelta(minutes=30),
         sql="""
         CREATE TABLE IF NOT EXISTS dim_ubicacion (
